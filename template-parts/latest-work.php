1778dcfb4251cdecf4bb3eabbfea49c9221bd6e5
Ash <ashley.m@divacreative.com>
2017-09-07 23:28:40 +0100

    added deploy file

--- template-parts/latest-work.php
+++ template-parts/latest-work.php
@@ -5,21 +5,21 @@
 	</div>
 
 	<div class="latest-inner"><?php
-			$custom_loop = new WP_Query(array( 'posts_per_page' => 8, 'post_type' => 'portfolio' ));
+			$custom_loop = new WP_Query(array( 'posts_per_page' => 4, 'post_type' => 'portfolio' ));
 			while ( $custom_loop->have_posts() ) : $custom_loop->the_post();  ?>
-					<a href="<?php the_permalink(); ?>">
+			
 						<div class="three columns latest-work">
 							<div class="latest-img">
 								<?php the_post_thumbnail('latest-port-size'); ?>
-								<div class="middle">
-									<h5><?php the_title(); ?></h5>
-								</div>
-							</div>
+														</div>
 						</div>
-					</a><?php
+				<?php
 
 			endwhile; 
 			wp_reset_postdata(); ?>
 	</div>
+</div>
 
+<div class="row twelve columns link">
+		<a href="/portfolio">View more<img src="<?php bloginfo('template_url') ?>/assets/img/blkArrowRight.png"></a>
 </div>
